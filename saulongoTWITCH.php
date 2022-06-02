f0fa56fcde9801ddce47ee43feee5eaa73b9842d
Daniel Orera <gorrodesigns@gmail.com>
2022-06-02 18:41:19 +0200

    si

--- saulongoTWITCH.php
+++
@@ -1,6 +0,0 @@
-<?php
-for ($i = 0; $i < 50; $i++) {
-    echo "saulongo en twitch suscribete ermo so<br>
-    saulongo en twitch suscribetewapo<br>
-    saulongo en twitch suscribete es grati<br>";
-}
